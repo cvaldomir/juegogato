18e3763af92782f7b44fc7dc26445a5001ff62e9
cvaldomir <christianvaldomircostanzo@hotmail.com>
2016-06-08 19:52:40 -0300

    fondo movimiento

--- flying_hero/game.rb
+++ flying_hero/game.rb
@@ -1,18 +1,23 @@
 require "gosu"
+require_relative "background"
+
 class Game < Gosu::Window
+  def initialize
+    super(988, 550, fullscreen: false)
+    self.caption = "Flying hero!"
+    @background = Background.new
+  end
 
-    def initialize
-         super(988, 550, fullscreen: false)
-         self.caption = "Flying hero!"
-     end
+  def draw
+    @background.draw
+  end
 
-    def draw
-    end	
-
-    def button_down(id)
-   			if id == Gosu::KbEscape
-   				close
-   			end	
-   	end			
-
+  def button_down(id)
+		if id == Gosu::KbEscape
+			close
+		end
+ 	end
+  def update
+    @background.scroll!
+  end
 end
